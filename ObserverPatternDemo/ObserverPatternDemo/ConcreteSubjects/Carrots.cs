b01e86bded262a36447eacd630d8013b0903a626
Sachin Kumar saini <sachinsaini_it@yahoo.co.in>
2021-12-24 20:14:39 +0530

    ObserverPattern

---
+++ ObserverPatternDemo/ObserverPatternDemo/ConcreteSubjects/Carrots.cs
@@ -0,0 +1,12 @@
+﻿using ObserverPatternDemo.Subject;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObserverPatternDemo.ConcreteSubjects
+{
+    class Carrots: Veggies
+    {
+        public Carrots(double price) : base(price) { }
+    }
+}
---
+++ ObserverPatternDemo/ObserverPatternDemo/Observer/IRestaurant.cs
@@ -0,0 +1,12 @@
+﻿using ObserverPatternDemo.Subject;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObserverPatternDemo.Observer
+{
+    public interface IRestaurant
+    {
+        void Update(Veggies veggies);
+    }
+}
